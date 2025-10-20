582961aea368ada4278c256004c0ab0fc7d6d90b
Luqmaan Sahar <luqmaan.sahar@gmail.com>
2025-10-20 14:38:06 +0100

    added docstrings

--- titanic_package/data_load.py
+++ titanic_package/data_load.py
@@ -5,6 +5,15 @@ TRAIN_PATH = "titanic_package/data/train.csv"
 TEST_PATH = "titanic_package/data/test.csv"
 
 def data_load(train_path=TRAIN_PATH, test_path=TEST_PATH):
+    """
+    Load both .csv datasets
+
+    Returns:
+    ----------
+    train_df: (pd.DataFrame) training data
+    test_df: (pd.(Dataframe) testing data
+
+    """
     train_df = pd.read_csv(train_path)
     test_df = pd.read_csv(test_path)
     return train_df, test_df
@@ -13,6 +22,15 @@ def data_load(train_path=TRAIN_PATH, test_path=TEST_PATH):
 def data_missing(df):
     """
     Returns the missing values per column/feature in the dataframe
+
+    Parameters:
+    ----------
+    df: (pd.DataFrame) dataset to summarise
+
+    Returns:
+    ----------
+    df_missing: (ndarray) array of summary statistics per feature
+
     """
     total = df.isnull().sum()
     percent = (df.isnull().sum()/df.isnull().count()*100)
@@ -22,13 +40,23 @@ def data_missing(df):
         dtype = str(df[col].dtype)
         types.append(dtype)
     tt['Types'] = types
-    df_missing_train = np.transpose(tt)
-    return df_missing_train
+    df_missing = np.transpose(tt)
+    return df_missing
 
 
 def data_frequent(df):
     """
     Returns a table summarising the most frequent data in each column/feature
+
+    Parameters:
+    ----------
+    df: (pd.DataFrame) dataset to summarise
+
+
+    Returns:
+    ----------
+    df_frequency: (ndarray) array of summary statistics per feature
+
     """
     total = df.count()
     tt = pd.DataFrame(total)
@@ -49,5 +77,31 @@ def data_frequent(df):
     tt['Most frequent item'] = items
     tt['Frequence'] = vals
     tt['Percent from total'] = np.round(vals / total * 100, 3)
-    output_df = np.transpose(tt)
-    return output_df
+    df_frequency = np.transpose(tt)
+    return df_frequency
+
+
+def data_unique(df):
+    """
+    Returns a table of the number of unique values per column/feature
+
+    Parameters:
+    ----------
+    df: (pd.DataFrame) dataset to summarise
+
+    
+    Returns:
+    ----------
+    df_unique: (ndarray) count of unique values for each column/feature
+    
+    """
+    total = df.count()
+    tt = pd.DataFrame(total)
+    tt.columns = ['Total']
+    uniques = []
+    for col in df.columns:
+        unique = df[col].nunique()
+        uniques.append(unique)
+    tt['Uniques'] = uniques
+    df_unique = np.transpose(tt)
+    return df_unique
